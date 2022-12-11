6f8cf5ef19e8980822b03bdd4c5dbca70a770fd2
Akshay <akshay.mhatre8989@gmail.com>
2022-12-11 06:14:05 +0530

    tests: adding e2e tests

---
+++ src/tests/index.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from "@playwright/test";
+
+test("tests", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(
+    "Akshay - Backbase Certifed Front End Developer"
+  );
+
+  await expect(page.getByTestId("header")).toHaveText("Hello");
+  await expect(page.getByTestId("personal-info")).toHaveText(
+    `I'am Akshay, Backbase Certified Front End Developer`
+  );
+  await expect(page.getByTestId("github-link")).toHaveAttribute(
+    "href",
+    "https://github.com/akshay3001"
+  );
+  await expect(page.getByTestId("linked-link")).toHaveAttribute(
+    "href",
+    "https://in.linkedin.com/in/akshay-mhatre-732a83144"
+  );
+});
