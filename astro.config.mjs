d28c7c854ce6678db12649857ca206d01f76d398
Akshay Mhatre <akshay.mhatre8989@gmail.com>
2023-04-04 12:06:11 +0200

    feat: configuring analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   site: "https://akshay-portfolio.vercel.app/",
-  integrations: [sitemap()],
+  integrations: [
+    sitemap(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
