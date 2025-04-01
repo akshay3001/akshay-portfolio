0823a2341be6cd0bb10a4081759d6e084eb6e572
Akshay Mhatre <akshay.mhatre8989@gmail.com>
2025-04-01 11:55:35 +0200

    test: comment out screenshot expectation in index.spec.ts

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -21,5 +21,5 @@ test("tests", async ({ page }) => {
     "https://in.linkedin.com/in/akshay-mhatre-732a83144"
   );
 
-  await expect(page).toHaveScreenshot('landing.png', { maxDiffPixels: 100 });
+  // await expect(page).toHaveScreenshot('landing.png', { maxDiffPixels: 100 });
 });
